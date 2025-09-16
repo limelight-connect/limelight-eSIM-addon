e2cb06a74b850f0b4d114a8a2b8f2d99d3925278
limelight <limelight@Limelight.limelight>
2025-09-16 03:26:08 -0400

    Update to version 1.1.38

--- docker/qmi.sh
+++ docker/qmi.sh
@@ -2,14 +2,20 @@
 
 CMD="$1"
 
-killall quectel-CM || exit 1
-
-[ "x${CMD}" = "xstop" ] && {
-    exit 0
+grep -E "^Y$" /sys/class/net/wwan0/qmi/raw_ip || {
+    echo Y > /sys/class/net/wwan0/qmi/raw_ip || exit 1
 }
 
+QMI_PID=$(ps -ef | grep quectel-CM | grep -v grep | awk '{print $2}')
+
+[ "x${CMD}" = "xstop" ] && {
+    [ "x${QMI_PID}" = "x" ] && exit 0
+    kill ${QMI_PID}
+    exit $?
+}
+
+[ "x${QMI_PID}" != "x" ] && kill ${QMI_PID}
+sleep 2
 /usr/bin/quectel-CM &
-
 sleep 5
-
 exit 0
